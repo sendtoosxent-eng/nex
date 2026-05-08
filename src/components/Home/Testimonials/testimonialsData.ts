96689b605a3773cbd5b46ff5f052d36fbaa0fab0
sendtoosxent-eng <sendtoosxent@gmail.com>
2026-05-08 14:06:14 +0300

    project upload

--- src/components/Home/Testimonials/testimonialsData.ts
+++ src/components/Home/Testimonials/testimonialsData.ts
@@ -2,43 +2,37 @@ import { Testimonial } from "@/types/testimonial";
 
 const testimonialsData: Testimonial[] = [
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Davis Dorwart",
     authorImg: "/images/users/user-01.jpg",
     authorRole: "Serial Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Wilson Dias",
     authorImg: "/images/users/user-02.jpg",
     authorRole: "Backend Developer",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Miracle Exterm",
     authorImg: "/images/users/user-03.jpg",
     authorRole: "Serial Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Thomas Frank",
     authorImg: "/images/users/user-01.jpg",
     authorRole: "Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Dave Smith",
     authorImg: "/images/users/user-02.jpg",
     authorRole: "Serial Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
+    review: `I recently shopped an Iphone 17 Pro Max at Nexcell Gadgets but i swear to you this is the best experience i have ever had with an online store, the customer service was top-notch and the delivery was super fast. I highly recommend Nexcell Gadgets to anyone looking for quality products and excellent service.`,
     authorName: "Davis Dorwart",
     authorImg: "/images/users/user-03.jpg",
     authorRole: "Serial Entrepreneur",
